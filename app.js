26b31632810a77c816a00ffdd7ec44740fb13532
Marcos Dias <marvcosta04@gmail.com>
2024-02-10 15:20:11 -0300

    Adicionando os pokemons da tela inicial

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 function getPokemonInfo(pokemonNameOrId) {
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`;
     
-    fetch(apiUrl)
+    return fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.json();
-        })
-        .then(data => {
-            displayPokemon(data);
         })
         .catch(error => {
             console.error('¡Se produjo un error al obtener la información del Pokémon:', error);
@@ -17,13 +14,16 @@ function getPokemonInfo(pokemonNameOrId) {
 }
 
 function displayPokemon(pokemonData) {
-    const pokemonContainer = document.getElementById('pokemon-container');
     const pokemonCard = document.createElement('div');
     pokemonCard.classList.add('pokemon-card');
-    pokemonCard.innerHTML = `
+    
+    const typeColor = getTypeColor(pokemonData.types[0].type.name); 
+    pokemonCard.style.borderTop = `5px solid ${typeColor}`;
+    pokemonCard.style.borderRadius = '8px';
+    pokemonCard.style.padding = '10px'; pokemonCard.innerHTML = `
         <h2>${pokemonData.name}</h2>
-        <p>nº: #00${pokemonData.id}</p>
-        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" />
+        <p>nº: #${pokemonData.id}</p>
+        <img src="${pokemonData.sprites.other.dream_world.front_default}" alt="${pokemonData.name}" />
         <p>Height: ${pokemonData.height}</p>
         <p>Weight: ${pokemonData.weight}</p>
         <h3>Types:</h3>
@@ -31,12 +31,60 @@ function displayPokemon(pokemonData) {
             ${pokemonData.types.map(type => `<li>${type.type.name}</li>`).join('')}
         </ul>
     `;
-    pokemonContainer.innerHTML = '';
-    pokemonContainer.appendChild(pokemonCard);
+    return pokemonCard;
 }
+
+function getTypeColor(type) {
+    const typeColors = {
+        normal: '#A8A878',
+        fire: '#F08030',
+        water: '#6890F0',
+        electric: '#F8D030',
+        grass: '#78C850',
+        ice: '#98D8D8',
+        fighting: '#C03028',
+        poison: '#A040A0',
+        ground: '#E0C068',
+        flying: '#A890F0',
+        psychic: '#F85888',
+        bug: '#A8B820',
+        rock: '#B8A038',
+        ghost: '#705898',
+        dragon: '#7038F8',
+        dark: '#705848',
+        steel: '#B8B8D0',
+        fairy: '#EE99AC'
+    };
+
+    return typeColors[type.toLowerCase()] || '#B0B0B0';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const pokemonContainer = document.getElementById('pokemon-container');
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=16')
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(pokemon => {
+                fetch(pokemon.url)
+                    .then(response => response.json())
+                    .then(pokemonData => {
+                        const pokemonCard = displayPokemon(pokemonData);
+                        pokemonContainer.appendChild(pokemonCard);
+                    });
+            });
+        })
+        .catch(error => console.error('Erro ao carregar os Pokémon:', error));
+});
 
 document.getElementById('search-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const searchTerm = document.getElementById('search-input').value;
-    getPokemonInfo(searchTerm.toLowerCase());
+    getPokemonInfo(searchTerm.toLowerCase())
+        .then(pokemonData => {
+            const pokemonContainer = document.getElementById('pokemon-container');
+            pokemonContainer.innerHTML = '';
+            const pokemonCard = displayPokemon(pokemonData);
+            pokemonContainer.appendChild(pokemonCard);
+        });
 });
